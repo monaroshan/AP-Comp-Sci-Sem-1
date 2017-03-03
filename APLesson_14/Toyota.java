d954bceb77d98f5bfc4511aa5f91694ab39b39ea
Mona Roshan <hithisismona@gmail.com>
2017-03-03 13:12:06 -0800

    toyota

--- APLesson_14/Toyota.java
+++ APLesson_14/Toyota.java
@@ -2,14 +2,18 @@ public class Toyota implements Location
 {
 	private double[] location;
 	
+	private double xCoord;
+	private double yCoord;
+	
 	public Toyota()
 	{
-		location = new double[];
+		location = new double[2];
 	}
 	
 	public Toyota(String c)
 	{
-		location = c.
+		xCoord = Double.parseDouble(c.substring(0,1));
+		yCoord = Double.parseDouble(c.substring(3));
 	}
 	
 	public int getID()
